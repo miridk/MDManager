1691a50a8aa4c0374d642e047ab6b563f8d89103
miridk <Rinkovski@live.dk>
2022-02-21 20:41:18 +0100

    started adding new template flow

---
+++ DevolopersMicroserviceGenerator/Design 4/AddNewTemplate.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design_4
+{
+    /// <summary>
+    /// Interaction logic for AddNewTemplate.xaml
+    /// </summary>
+    public partial class AddNewTemplate : Page
+    {
+        public AddNewTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void findNugetFileBtn(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    lbFiles.Items.Add(Path.GetFileName(filename));
+            }
+        }
+    }
+}
